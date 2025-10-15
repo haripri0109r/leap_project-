366b0fd640755619124ef44aa5c95289f87b1573
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-15 23:05:35 +0530

    feat:completed the userController and userService ->with the required functions to perform in DB.

--- src/main/java/com/example/Online/Book/Store/repository/UserReposistory.java
+++
@@ -1,5 +0,0 @@
-package com.example.Online.Book.Store.repository;
-
-public class UserReposistory {
-    
-}
--- src/main/java/com/example/Online/Book/Store/service/UserService.java
+++ src/main/java/com/example/Online/Book/Store/service/UserService.java
@@ -1,5 +1,40 @@
 package com.example.Online.Book.Store.service;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.Online.Book.Store.entity.User;
+import com.example.Online.Book.Store.repository.UserRepository;
+
+@Service
 public class UserService {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // public User updateUser(Long id, User userDetails) {
+    //     User user = userRepository.findById(id).orElseThrow();
+    //     user.setName(userDetails.getName());
+    //     user.setEmail(userDetails.getEmail());
+    //     return userRepository.save(user);
+    // }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
