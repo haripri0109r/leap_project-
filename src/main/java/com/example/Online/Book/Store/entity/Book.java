aa4ae8fb211347caeaac8ebc7e257d1db7920f1f
haripri0109r <haripri0109r@gmail.com>
2025-10-15 22:43:02 +0530

    feat:Book

--- src/main/java/com/example/Online/Book/Store/entity/Book.java
+++ src/main/java/com/example/Online/Book/Store/entity/Book.java
@@ -1,5 +1,31 @@
 package com.example.Online.Book.Store.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+
+@Data
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private int rating;
+    private Long  stock;
+    private double price;
+
     
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Order> orders;
 }
