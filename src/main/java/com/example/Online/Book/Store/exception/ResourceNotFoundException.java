48822859b515a1cb1b70b0863d715796ca38d3a1
haripri0109r <haripri0109r@gmail.com>
2025-10-15 22:52:23 +0530

    feat:ResoureNotFoundException

--- src/main/java/com/example/Online/Book/Store/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/Online/Book/Store/exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.example.Online.Book.Store.exception;
 
-public class ResourceNotFoundException {
-    
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
