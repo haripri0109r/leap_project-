2c7ccf8d1d6a1a640c450b040dbdee2f13daae31
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-15 22:08:13 +0530

    feat:User.java has been initialised with required field names.

--- src/main/java/com/example/Online/Book/Store/entity/User.java
+++ src/main/java/com/example/Online/Book/Store/entity/User.java
@@ -1,5 +1,17 @@
 package com.example.Online.Book.Store.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String user_name;
+    private String email;
+    private String password;
+    private Long  phone_no;
 }
